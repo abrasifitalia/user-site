cab395505f5d6c2f99ac66b52b99f5429a1ec362
abrasifitalia <abrasifitalia@gmail.com>
2025-01-17 13:41:30 +0100

    v1.2.2+2

--- src/components/Home/location.js
+++ src/components/Home/location.js
@@ -14,15 +14,22 @@ const CompanyLocation = () => {
           <p className="text-gray-700 mb-2">
             <strong>Adresse:</strong> Croisement La Soukra
           </p>
-          <p className="text-gray-700 mb-4">
-            <strong>Téléphone:</strong>{" "}
-            <a
-              href="tel:+21620235829"
-              className="text-green-600 hover:text-green-800 transition-colors"
-            >
-              +216 20 235 829
-            </a>
-          </p>
+          {[
+            { label: "Téléphone", number: "+216 55 888 111" },
+            { label: "Téléphone 2", number: "+216 20 235 829" },
+            { label: "Téléphone 3", number: "+216 94 615 025" },
+            { label: "Téléphone 4", number: "+216 22 107 207" },
+          ].map((phone, index) => (
+            <p className="text-white mb-4 bg-success p-2 rounded-lg" key={index}>
+              <strong>{phone.label} :</strong>{" "}
+              <a
+                href={`tel:${phone.number.replace(/\s/g, '')}`}
+                className="text-white hover:text-green-800 transition-colors"
+              >
+                {phone.number}
+              </a>
+            </p>
+          ))}
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d822826.3192766692!2d9.7467303!3d36.3325432!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12e2cb32b001ae29%3A0x8599646157513aca!2sAbrasif%20Italia%20Klindex!5e0!3m2!1sfr!2stn!4v1737038397640!5m2!1sfr!2stn"
             width="100%"
@@ -41,15 +48,20 @@ const CompanyLocation = () => {
           <p className="text-gray-700 mb-2">
             <strong>Adresse:</strong> Bouhssina, Cité Boukhzar
           </p>
-          <p className="text-gray-700 mb-4">
-            <strong>Téléphone:</strong>{" "}
-            <a
-              href="tel:+21658982743"
-              className="text-green-600 hover:text-green-800 transition-colors"
-            >
-              +216 58 982 743
-            </a>
-          </p>
+          {[
+            { label: "Téléphone 1", number: "+216 58 982 743" },
+            { label: "Téléphone 2", number: "+216 55 334 131" },
+          ].map((phone, index) => (
+            <p className="text-white mb-4 bg-success p-2 rounded-lg" key={index}>
+              <strong>{phone.label} :</strong>{" "}
+              <a
+                href={`tel:${phone.number.replace(/\s/g, '')}`}
+                className="text-white hover:text-green-800 transition-colors"
+              >
+                {phone.number}
+              </a>
+            </p>
+          ))}
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1161607.3130379696!2d9.407760752335607!3d36.46922189368688!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fd8b81f9527ed5%3A0x3eeac41c8017c1de!2sAbrasif%20italia!5e0!3m2!1sfr!2stn!4v1737038425440!5m2!1sfr!2stn"
             width="100%"
