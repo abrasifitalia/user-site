5d22c0fea0609719d82a6b5f06835327f59d603f
abrasifitalia <abrasifitalia@gmail.com>
2025-02-07 21:48:26 +0100

    v1.3.7 : update footer with new social media links and add pro mails

--- src/components/includes/footer.js
+++ src/components/includes/footer.js
@@ -40,9 +40,10 @@ const Footer = () => {
         <div>
           <h4 className="text-lg font-semibold mb-4">Réseaux sociaux</h4>
           <ul className="space-y-2 text-gray-400">
-            <li><CiInstagram className="text-success text-2xl "/> <a href="https://www.instagram.com/abrasif_italia_klindex/" className="text-gray-400 link-underline link-underline-opacity-0"> <span className="font-bold text-gray-400">Abrasif Italia Klindex</span></a></li>
-            <li><CiFacebook className="text-success text-2xl font-bold"/> <a href="https://www.facebook.com/people/Abrasif-Italia-Klindex/100057219229918/" className="text-gray-400 link-underline link-underline-opacity-0"> <span className="font-bold text-gray-400">Facebook: Abrasif Italia Klindex</span></a></li>
-            <li><CiMail className="text-success text-2xl font-bold"/> <a href="mailto:abrasif.italia3@gmail.com" className="text-gray-400 link-underline link-underline-opacity-0"> <span className="font-bold text-gray-400"> abrasif.italia@gmail.com</span></a></li>
+            <li><CiInstagram className="text-success text-2xl "/> <a href="https://www.instagram.com/abrasif_italia_hg/" className="text-gray-400 link-underline link-underline-opacity-0"> <span className="font-bold text-gray-400">Abrasif Italia HG</span></a></li>
+            <li><CiFacebook className="text-success text-2xl font-bold"/> <a href="https://www.facebook.com/profile.php?id=100057219229918" className="text-gray-400 link-underline link-underline-opacity-0"> <span className="font-bold text-gray-400"> Abrasif Italia HG</span></a></li>
+            <li><CiMail className="text-success text-2xl font-bold"/> <a href="mailto:soukra@abrasifitalia.com" className="text-gray-400 link-underline link-underline-opacity-0"> <span className="font-bold text-gray-400">soukra@abrasifitalia.com</span></a></li>
+            <li><CiMail className="text-success text-2xl font-bold"/> <a href="mailto:sousse@abrasifitalia.com" className="text-gray-400 link-underline link-underline-opacity-0"> <span className="font-bold text-gray-400">sousse@abrasifitalia.com</span></a></li>
             <li><CiMail className="text-success text-2xl font-bold"/> <a href="mailto:abrasif.italia3@gmail.com" className="text-gray-400 link-underline link-underline-opacity-0"> <span className="font-bold text-gray-400"> directeur@abrasifitalia.com</span></a></li>
             
           </ul>
